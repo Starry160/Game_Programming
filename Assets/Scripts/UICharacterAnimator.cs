9752c3d94fa40df216e3846c30aac35897acaa50
Starry160 <3241321796@qq.com>
2026-06-06 13:48:45 +0800

    Refine the character and weapon UI in the main menu interface

--- Assets/Scripts/UICharacterAnimator.cs
+++ Assets/Scripts/UICharacterAnimator.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>UI 角色辅助：保证 Image 以原始像素尺寸显示。</summary>
+[ExecuteAlways]
 [RequireComponent(typeof(Image))]
 public class UICharacterAnimator : MonoBehaviour
 {
     private Image _image;
 
+    private void OnEnable()
+    {
+        RefreshNativeSize();
+    }
+
     private void Awake()
     {
         _image = GetComponent<Image>();
+        RefreshNativeSize();
+    }
+
+    private void OnValidate()
+    {
         RefreshNativeSize();
     }
 
